094ba230d1c35824d690ef843a8feb33c6538a99
Bahae <bahaehamrit65@gmail.com>
2022-06-24 16:32:56 +0100

    added octopuce project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.{html,js}"],
+  content: [
+    "*.{html,js}",
+    "./projects/octopuce/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
